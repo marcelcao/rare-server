a4bd1bd4fa6fe94639dab6040659966fab8bfa5d
Emily “Lemony” Drew L <126979112+EmilyLemonyDrewL@users.noreply.github.com>
2023-11-04 08:16:30 -0600

    Update request_handler.py
    line 118 in request_handler.py changed back to reflect main

--- request_handler.py
+++ request_handler.py
@@ -115,7 +115,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('content-length', 0))
         post_body = json.loads(self.rfile.read(content_len))
         response = ''
-        (resource, id) = self.parse_url()
+        resource, _ = self.parse_url()
 
         if resource == 'login':
             response = login_user(post_body)
