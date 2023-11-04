3148905f5630b54c3dfd39d8af369ed28e0b3a71
Mara Caoile <mara.caoile@gmail.com>
2023-11-04 13:22:49 -0500

    refactor

--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from urllib.parse import urlparse, parse_qs
 from views.user import create_user, login_user
 from views import create_post, update_post, delete_post, get_all_posts, get_posts_by_user_id, get_single_post
 from views import get_all_tags, get_single_tag, create_tag, delete_tag, update_tag
 from views import get_all_post_tags, get_single_post_tag, create_post_tag, delete_post_tag, update_post_tag
-from urllib.parse import urlparse, parse_qs
 from views import (
     get_all_reactions,
     get_single_reaction,
